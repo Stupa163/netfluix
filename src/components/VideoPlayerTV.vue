f8edf4b5c755d4ef5958a5d46e1287ab638dbdc5
a <a>
2020-10-22 13:04:44 +0200

    Home page

---
+++ src/components/VideoPlayerTV.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="video_player">
+        <img src="../assets/tv.png" alt="">
+        <div class="video">
+            <video ref="video_ref" muted loop/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "VideoPlayerTV",
+        mounted: function () {
+            this.$refs.video_ref.src = 'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v';
+            this.$refs.video_ref.play();
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .video_player {
+        position: relative;
+
+        img {
+            position: relative;
+            z-index: 9999;
+        }
+
+        .video {
+            position: absolute;
+            top: 97px;
+            left: 84px
+        }
+    }
+</style>
