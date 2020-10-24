a456a8be033e45ed2f77043335a179d3dd0ecd16
Nico <minecraft77boss@gmail.com>
2020-10-24 16:49:17 +0200

    feat: Ajout store feat: Ajout URL gravatar feat: Passage de vue en mode history feat: Ajout des fonctions d'authentification

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import store from './store/index.js'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
