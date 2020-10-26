7267b40026d887e5b93994718c1a188aedbf774e
a <a>
2020-10-26 18:52:43 +0100

    Fct : Added a default image if no poster_path is provided

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -2,5 +2,6 @@ export default {
     apiUrl: process.env.VUE_APP_API_URL,
     apiKey: process.env.VUE_APP_API_KEY,
     apiGravatarUrl: process.env.VUE_APP_API_GRAVATAR_URL,
-    apiAuthentificationUrl: process.env.VUE_APP_API_AUTHENTIFICATION_URL
+    apiAuthentificationUrl: process.env.VUE_APP_API_AUTHENTIFICATION_URL,
+    defaultPosterImage: process.env.VUE_APP_DEFAULT_COVER_IMAGE
 }
