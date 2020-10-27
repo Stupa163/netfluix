ece0034254e770f0344b9f289a7b991a277754d5
NICO <test@gmail.com>
2020-10-27 23:23:39 +0100

    refactor: Déplacement fonction watchlist dans le store vuex refactor: Changement d'appelle de fonction par des mapActions

--- src/store/index.js
+++ src/store/index.js
@@ -8,123 +8,196 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: {
-      session_id: localStorage.getItem('session_id') || null,
+      session_id: localStorage.getItem("session_id") || null,
       account_id: null,
       username: "",
-      avatar_url: ""
-    }
+      avatar_url: "",
+    },
   },
   mutations: {
     setSessionID(state, session_id) {
-      state.user.session_id = session_id
+      state.user.session_id = session_id;
     },
 
     setUser(state, user) {
-      state.user.username = user.username
-      state.user.avatar_url = user.avatar.gravatar.hash
-      state.user.account_id = user.id
-    }
+      state.user.username = user.username;
+      state.user.avatar_url = user.avatar.gravatar.hash;
+      state.user.account_id = user.id;
+    },
   },
   getters: {
     isLogged(state) {
-      return state.user.session_id != null
+      return state.user.session_id != null;
     },
     getUsername(state) {
-      return state.user.username
+      return state.user.username;
     },
     getAvatarURL(state) {
-      return state.user.avatar_url
+      return state.user.avatar_url;
     },
     getSessionID(state) {
-      return state.user.session_id
+      return state.user.session_id;
     },
     getAccountID(state) {
-      return state.user.account_id
-    }
+      return state.user.account_id;
+    },
   },
   actions: {
-
+    /* Authentification (A mettre dans un module)*/
     async getRequestToken(context) {
-      console.log('📜 Récup token...')
+      console.log("📜 Récup token...");
 
-      return fetch(`${APIConfig.apiUrl}/authentication/token/new?api_key=${APIConfig.apiKey}`)
+      return fetch(
+        `${APIConfig.apiUrl}/authentication/token/new?api_key=${APIConfig.apiKey}`
+      )
         .then((response) => response.json())
-        .then((json) => { return json })
-        .catch((err) => { console.error(err) })
+        .then((json) => {
+          return json;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     },
-
     async getAuthorization({ dispatch }) {
       //Demande de récupération du request_token pour création du lien d'autorisation
-      let request_token = await dispatch('getRequestToken')
+      let request_token = await dispatch("getRequestToken");
 
       //Redirection vers le lien d'autorisation TMDB
-      window.location.href = `${APIConfig.apiAuthentificationUrl}${request_token.request_token}?redirect_to=${window.location.origin + router.resolve({name: 'Login'}).href}`
+      window.location.href = `${APIConfig.apiAuthentificationUrl}${
+        request_token.request_token
+      }?redirect_to=${window.location.origin +
+        router.resolve({ name: "Login" }).href}`;
     },
-
     async getSessionID(context, request_token) {
-
       //Récupération de la session_id
-      return fetch(`${APIConfig.apiUrl}/authentication/session/new?api_key=${APIConfig.apiKey}&request_token=${request_token}`, { method: 'POST' })
+      return fetch(
+        `${APIConfig.apiUrl}/authentication/session/new?api_key=${APIConfig.apiKey}&request_token=${request_token}`,
+        { method: "POST" }
+      )
         .then((response) => response.json())
-          .then((json) => {
-
-          console.log('✔️ Session ID obtenue !')
+        .then((json) => {
+          console.log("✔️ Session ID obtenue !");
 
           //Stockage dans le local storage
-          localStorage.setItem('session_id', json.session_id)
+          localStorage.setItem("session_id", json.session_id);
 
           //Stockage dans store
-          context.commit('setSessionID', json.session_id)
+          context.commit("setSessionID", json.session_id);
 
           //Return du session ID
-          return json.session_id
-
+          return json.session_id;
         })
-        .catch((err) => console.error(err))
+        .catch((err) => console.error(err));
     },
-
     async getUserAccount(context, session_id) {
-
       //Récupération infos user
-      return fetch(`${APIConfig.apiUrl}/account?api_key=${APIConfig.apiKey}&session_id=${session_id}`)
+      return fetch(
+        `${APIConfig.apiUrl}/account?api_key=${APIConfig.apiKey}&session_id=${session_id}`
+      )
         .then((response) => response.json())
-          .then((json) => {
-          console.log('✔️ Infos utilisateur obtenue !')
-          console.log(json)
+        .then((json) => {
+          console.log("✔️ Infos utilisateur obtenue !");
+          console.log(json);
 
           //Mise à jour objet user
-          context.commit('setUser', json)
+          context.commit("setUser", json);
 
           //Return de l'objet 'user'
-          return json
+          return json;
         })
-        .catch((err) => console.error(err))
+        .catch((err) => console.error(err));
     },
-
     async getLogout(context, session_id) {
       //Déconnexion de l'utilisateur de l'API de TMDB
 
       let options = {
-        method: 'DELETE',
-        body: JSON.stringify({ "session_id": session_id }),
+        method: "DELETE",
+        body: JSON.stringify({ session_id: session_id }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }
+          "Content-Type": "application/json",
+        },
+      };
 
-      return fetch(`${APIConfig.apiUrl}/authentication/session?api_key=${APIConfig.apiKey}`, options)
-        .then(response => response.json())
-          .then(json => {
-          console.log('✔️ Utilisateur déconnecté !')
-          console.log(json)
+      return fetch(
+        `${APIConfig.apiUrl}/authentication/session?api_key=${APIConfig.apiKey}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("✔️ Utilisateur déconnecté !");
+          console.log(json);
 
           //Supprimé du localstorage
-          localStorage.removeItem('session_id')
+          localStorage.removeItem("session_id");
 
           //Redirection de l'utilisateur vers la page home
-          window.location.reload()
+          window.location.reload();
         })
-        .catch(err => console.error(err))
-    }
-  }
-})
+        .catch((err) => console.error(err));
+    },
+
+    /* Watchlist (A mettre dans un module) */
+    async addMovieWatchlist(context, payload) {
+      let options = {
+        method: "POST",
+        body: JSON.stringify({
+          media_type: "movie",
+          media_id: payload.media_id,
+          watchlist: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      return fetch(
+        `${APIConfig.apiUrl}/account/${payload.account_id}/watchlist?api_key=${APIConfig.apiKey}&session_id=${payload.session_id}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+        })
+        .catch((err) => console.error(err));
+    },
+    async deleteMovieWatchlist(context, payload) {
+      let options = {
+        method: "POST",
+        body: JSON.stringify({
+          media_type: "movie",
+          media_id: payload.media_id,
+          watchlist: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      return fetch(
+        `${APIConfig.apiUrl}/account/${payload.account_id}/watchlist?api_key=${APIConfig.apiKey}&session_id=${payload.session_id}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return {
+            code: 200,
+            message: "Film supprimé de la watchlist avec succés !",
+          };
+        })
+        .catch((err) => console.error(err));
+    },
+    async getWatchlistMovie(context, payload) {
+      return fetch(
+        `${APIConfig.apiUrl}/account/${payload.account_id}/watchlist/movies?api_key=${APIConfig.apiKey}&language=fr-FR&session_id=${payload.session_id}`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("✔️ Watchlist obtenue !");
+          console.log(json);
+          return json;
+        })
+        .catch((err) => console.error(err));
+    },
+  },
+});
--- src/views/Watchlist.vue
+++ src/views/Watchlist.vue
@@ -8,6 +8,7 @@
     <b-row>
       <b-col>
         <h1 class="text-light" style="margin-left: 10vh">Mes séries...</h1>
+        <b-button variant="danger" @click="addMovieWatchlist({account_id: getAccountID,session_id: getSessionID, media_id: 13})">Test</b-button>
       </b-col>
     </b-row>
     <b-row>
@@ -19,7 +20,7 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters,mapActions } from "vuex";
 import MovieList from "../components/MovieList";
 import APIConfig from "../config/api.config";
 
@@ -32,71 +33,17 @@ export default {
   },
   components: { MovieList },
   methods: {
-    async getWatchlistMovie(account_id, session_id) {
-      return fetch(
-        `${APIConfig.apiUrl}/account/${account_id}/watchlist/movies?api_key=${APIConfig.apiKey}&language=fr-FR&session_id=${session_id}`
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          console.log("✔️ Watchlist obtenue !");
-          console.log(json);
-          return json;
-        })
-        .catch((err) => console.error(err));
-    },
-    async addMovieWatchlist(account_id, session_id, media_id) {
-      let options = {
-        method: "POST",
-        body: JSON.stringify({
-          media_type: "movie",
-          media_id: media_id,
-          watchlist: true,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      return fetch(
-        `${APIConfig.apiUrl}/account/${account_id}/watchlist?api_key=${APIConfig.apiKey}&session_id=${session_id}`,
-        options
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-        })
-        .catch((err) => console.error(err));
-    },
-    async deleteMovieWatchlist(account_id, session_id, media_id) {
-      let options = {
-        method: "POST",
-        body: JSON.stringify({
-          media_type: "movie",
-          media_id: media_id,
-          watchlist: false,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      return fetch(
-        `${APIConfig.apiUrl}/account/${account_id}/watchlist?api_key=${APIConfig.apiKey}&session_id=${session_id}`,
-        options
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-        })
-        .catch((err) => console.error(err));
-    },
+    ...mapActions([
+      "addMovieWatchlist",
+      "getWatchlistMovie"
+    ])
   },
   computed: {
     ...mapGetters(["getAccountID", "getSessionID"]),
   },
 
   async mounted() {
-    this.getWatchlistMovie(this.getAccountID, this.getSessionID).then(
+    this.getWatchlistMovie({'account_id': this.getAccountID, 'session_id': this.getSessionID}).then(
       (res) => (this.watchlist = res.results)
     );
   },
