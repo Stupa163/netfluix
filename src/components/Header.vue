c6f643ed3267f94e474de5e195f9e0a9c4c7d499
Nico <minecraft77boss@gmail.com>
2020-10-30 15:18:33 +0100

    feat: Ajout logo minifié de l'header style: Changement de l'header pour la navigation mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,21 +6,17 @@
       fixed="top"
       style="background-image: linear-gradient(0deg,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 1) 100%);"
     >
+      <!-- Logo version bureau/mobile -->
       <b-navbar-brand>
         <router-link :to="{'name': 'Home'}">
-          <img src="../assets/logo.png" alt="logo" width="160vh" />
+          <img class="disabled-logo" src="../assets/logo.png" alt="logo" width="160vh" />
+          <img class="minimize-logo" src="../assets/logo-2.png" alt="logo" width="35vh" />
         </router-link>
       </b-navbar-brand>
+      <!-- ./logo -->
 
-      <b-navbar-nav v-if="isLogged">
-        <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Latest' ? true : false}" :to="{'name': 'Latest'}">Nouveautées</b-nav-item>
-        <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Watchlist' ? true : false}" :to="{'name': 'Watchlist'}">Mes séries suivie</b-nav-item>
-        <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Search' ? true : false}" :to="{'name': 'Search'}">Rechercher un film</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <!-- Si pas connecté -->
+      <b-navbar-nav>
+        <!-- Si pas connecté affichage-->
         <b-button
           v-if="!isLogged"
           size="md"
@@ -29,7 +25,7 @@
           >S'indentifier via TMDB</b-button>
 
         <!-- Sinon affichage du profil utilisateur -->
-        <div v-else>
+        <div v-else class="disabled-logo">
           <b-nav-text class="text-light">{{getUsername}}</b-nav-text>
           <b-dropdown
           right
@@ -62,9 +58,33 @@
           <b-dropdown-item @click="logout(getSessionID)"><span class="font-weight-bold">Se déconnecter</span></b-dropdown-item>
         </b-dropdown>
         </div>
-
       </b-navbar-nav>
-      <!-- ./right-nav-items -->
+
+      <!-- Bouton menu version mobile -->
+      <b-navbar-toggle target="nav-collapse" class="no-border">
+         <template>
+            <img
+              :src="'https://www.gravatar.com/avatar/'+ getAvatarURL"
+              class="rounded-circle"
+              width="35vh"
+              height="35vh"
+              alt=""
+            />
+         </template>
+      </b-navbar-toggle>
+      <!-- ./bouton-menu-mobile -->
+
+      <!-- Navigation version mobile -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav v-if="isLogged">
+          <b-dropdown-item @click="logout(getSessionID)" class="minimize-logo"><span class="font-weight-bold">Se déconnecter</span></b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Latest' ? true : false}" :to="{'name': 'Latest'}">Nouveautées</b-nav-item>
+          <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Watchlist' ? true : false}" :to="{'name': 'Watchlist'}">Mes séries suivie</b-nav-item>
+          <b-nav-item :class="{'pr-3': true,'h5': true, 'active': this.$route.name === 'Search' ? true : false}" :to="{'name': 'Search'}">Rechercher un film</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+      <!-- ./navigation-mobile -->
 
     </b-navbar>
   </div>
@@ -74,18 +94,36 @@
 import { mapActions } from "vuex";
 import { mapGetters } from "vuex";
 import APIConfig from "../config/api.config";
+import { BIconChevronBarDown, BIconChevronBarUp } from "bootstrap-vue";
+import Utils from "../mixins/Utils";
 
 export default {
   name: "Header",
+  data: function () {
+    return {
+      isMobiles: false,
+    };
+  },
   methods: {
     ...mapActions({
       login: "getAuthorization",
       logout: "getLogout",
     }),
   },
+  components: {
+    BIconChevronBarDown,
+    BIconChevronBarUp,
+  },
+  mixins: [Utils],
   computed: {
     ...mapGetters(["getUsername", "isLogged", "getAvatarURL", "getSessionID"]),
   },
+  watch: {
+    isMobile: () => {
+      //Check du support utilisé
+      this.isMobiles = this.isMobile();
+    }
+  }
 };
 </script>
 
@@ -100,19 +138,31 @@ export default {
   border: none;
 }
 
-ul.dropdown-menu{
-  opacity: 0.80 !important;
+/* ul.dropdown-menu {
+  opacity: 0.8 !important;
   background-color: rgb(22, 22, 22) !important;
   border-radius: 2px !important;
   border: 1px solid rgb(65, 65, 65) !important;
 }
 
-.dropdown-menu>li>a{
+.dropdown-menu > li > a {
   color: white !important;
-    &:hover{
-      transition: background-color 0.3s ease-in-out;
-      background-color: rgb(51, 51, 51);
-      color: white !important;
-    }
+  &:hover {
+    transition: background-color 0.3s ease-in-out;
+    background-color: rgb(51, 51, 51);
+    color: white !important;
+  }
+} */
+
+.disabled-logo {
+  @media screen and (max-width: 990px) {
+    display: none;
+  }
+}
+
+.minimize-logo {
+  @media screen and (min-width: 991px) {
+    display: none;
+  }
 }
 </style>
