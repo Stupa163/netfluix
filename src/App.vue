a22582dd4da8022eb3f469b6c30267f0b70f87d5
a <a>
2020-10-30 17:49:18 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@
   import Header from "./components/Header";
   import Footer from "./components/Footer";
 
+  import '../registerServiceWorker'
+
   export default {
     components: {Header, Footer},
     created() {
